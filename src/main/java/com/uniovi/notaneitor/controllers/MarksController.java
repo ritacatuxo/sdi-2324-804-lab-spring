7576161341478245af777a7e1ee41670d3c13a46
Rita Catuxo <uo284185@uniovi.es>
2024-02-01 22:13:35 +0100

    SDI-2324-804-2.5-Vistas, motor de plantillas Thymeleaf.

--- src/main/java/com/uniovi/notaneitor/controllers/MarksController.java
+++ src/main/java/com/uniovi/notaneitor/controllers/MarksController.java
@@ -3,9 +3,11 @@ package com.uniovi.notaneitor.controllers;
 import com.uniovi.notaneitor.entities.Mark;
 import com.uniovi.notaneitor.services.MarksService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RestController // responde a peticiones Rest
+@Controller // responde a peticiones Rest
 public class MarksController {
 
     // declarar que el servicio MarksService va a ser usado desde este controlador (Inyectar el servicio)
@@ -13,29 +15,53 @@ public class MarksController {
     private MarksService marksService;
 
 
-    // un método por cada URL a la que responde el controlador
 
+    // un método por cada URL a la que responde el controlador
     @RequestMapping("/mark/list")
-    public String getList() {
-        return marksService.getMarks().toString();
+    public String getList(Model model) {
+        model.addAttribute("markList", marksService.getMarks());
+        return "mark/list";
+        //Cómo es que list.html tiene acceso al objeto 'model' (y por tanto a markList)? si solo esta en el método getList(Model model)
     }
 
+    @RequestMapping(value = "/mark/add")
+    public String getMark() {
+        return "mark/add";
+    }
     @RequestMapping(value = "/mark/add", method = RequestMethod.POST)
     public String setMark(@ModelAttribute Mark mark) {
         marksService.addMark(mark);
-        return "Ok";
+        return "redirect:/mark/list";
+
     }
 
 
     // petición GET y que pueda recibir un parámetro id.
     @RequestMapping("/mark/details/{id}")
-    public String getDetail(@PathVariable Long id) {
-        return marksService.getMark(id).toString();
+    public String getDetail(Model model, @PathVariable Long id) {
+        model.addAttribute("mark", marksService.getMark(id));
+        return "mark/details";
     }
 
     @RequestMapping("/mark/delete/{id}")
     public String deleteMark(@PathVariable Long id){
         marksService.deleteMark(id);
-        return "Ok";
+        return "redirect:/mark/list";
+
     }
+
+    @RequestMapping(value = "/mark/edit/{id}")
+    public String getEdit(Model model, @PathVariable Long id) {
+        model.addAttribute("mark", marksService.getMark(id));
+        return "mark/edit";
+    }
+    @RequestMapping(value="/mark/edit/{id}", method=RequestMethod.POST)
+    public String setEdit(@ModelAttribute Mark mark, @PathVariable Long id){
+        // de dónde sale el primer parametro? en que lugar se lo pasan?
+        // no entiendo lo de que ese mark (el primer parametro) sea el resultado del formulario de edit.html, ya que lo unico que mete el usuario es la descripcion y el score. en que lugar se crea el objeto en si?
+        mark.setId(id);
+        marksService.addMark(mark);
+        return "redirect:/mark/details/"+id;
+    }
+
 }
